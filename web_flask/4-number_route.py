2057e126612a099e0e238ef15e46e117213ce21d
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-23 17:38:52 +0300

    flask web app

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This script starts a simple flask web application"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """This function displays a text"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This function displays a text"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """This function takes a variable and displays text containing it"""
+    return 'C {}'.format(escape(text).replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_text(text):
+    """This function takes a variable and displays text containing it"""
+    return 'Python {}'.format(escape(text).replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """This function displays '<value of n> is a number' """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
