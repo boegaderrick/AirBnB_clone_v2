bc7d30c4b6ca524257e6bffce82beaa6adc39eac
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-23 14:17:11 +0300

    flask web app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,8 @@
 # This script starts a simple flask web application
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
