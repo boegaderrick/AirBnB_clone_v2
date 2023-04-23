7558b4c0400a0e3144b51b7135b478d0df11b4d2
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-23 22:09:05 +0300

    storage engines update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -29,7 +29,7 @@ class DBStorage:
 
     def close(self):
         """This method discards a session object"""
-        self.__session.close()
+        self.__session.remove()
 
     def new(self, obj):
         """This method adds an object to the database"""
