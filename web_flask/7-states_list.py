c11ef32f2b375751321f6cd31ed93a1c0a3b6367
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-04-25 12:14:56 +0300

    storage engines update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """This script starts a simple flask web application"""
 from flask import Flask, escape, render_template
-# from models import storage
-# from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """This function renders a html page containing list of states"""
-    from models import storage
-    from models.state import State
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(Exception=None):
+def teardown(self, Exception=None):
     """This function closes an sqlalchemy session"""
-    from models import storage
     storage.close()
 
 
 if __name__ == '__main__':
+    from models import storage
+    from models.state import State
     app.run(host='0.0.0.0', port=5000)
